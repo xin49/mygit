f24b4ed58707164429e2d571a3b990171682edea
xin49 <nnmealy@gmail.com>
2016-10-16 22:16:58 +0800

    2016年10月16日 星期日 22时17分02秒

---
+++ cpp/simpleTest/classAndObj.cpp
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: classAndObj.cpp
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+class TBox{
+public:
+    double length;
+    double breadth;
+    double height;
+};
+
+int main()
+{
+    TBox box1, box2;
+
+    cout << box1.length << endl;
+    return 1;
+}
