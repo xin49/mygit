2f0048d427297be268135b93808e4d14848dbc4e
xin49 <nnmealy@gmail.com>
2016-10-17 23:11:21 +0800

    2016年10月17日 星期一 23时11分26秒

--- cpp/simpleTest/classAndObj.cpp
+++ cpp/simpleTest/classAndObj.cpp
@@ -2,20 +2,101 @@
 	> File Name: classAndObj.cpp
  ************************************************************************/
 
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
-class TBox{
+class TDuck;
+typedef TDuck* PDuck;
+class TMyDuck;
+typedef TMyDuck* PMyDuck;
+
+class TDuck{
+
 public:
-    double length;
-    double breadth;
-    double height;
+    TDuck();
+    ~TDuck();
+
+    int color;
+
+    void Speak(void)
+    {
+        cout << "Gua Gua Gua~~!!!" << endl;
+    }
+
+    void Swim(bool hasWater)
+    {
+        if(hasWater)
+            Speak();
+    }
+
+    //member function declarations
+    bool Fly(void);
+    int GetAge();
+protected:
+//protected members
+    bool hasBaby;
+private:
+    int fAge;
+    PDuck fPPar;
+    PDuck fPMar;
 };
+
+int TDuck::GetAge()
+{
+    return fAge;    
+}
+
+//member function definition
+bool TDuck::Fly(void)
+{
+    if(fAge >= 5)
+        return true;
+}
+
+TDuck::TDuck()
+{
+    fAge = 1;
+    color = rand();
+}
+
+TDuck::~TDuck()
+{
+    cout << "Ender's Game"<< endl;    
+}
+
+class TMyDuck: public TDuck
+{
+public:
+    TMyDuck();
+    ~TMyDuck();
+    void FindBeautyDuck(bool find)
+    {
+        //fAge = 19; inherit can not use fAge
+        if(find)
+            hasBaby = true;
+        else
+            hasBaby = false;
+    }
+};
+
+TMyDuck::TMyDuck()
+{
+}
+
+TMyDuck::~TMyDuck()
+{
+}
 
 int main()
 {
-    TBox box1, box2;
+    PMyDuck pYaYa = new TMyDuck;
+    if(pYaYa->Fly())
+        cout << "I can fly~~!!!" << endl;
+    else
+        cout << "Game over~~!!!" << endl;
+    cout << pYaYa->GetAge() << endl;
+    free(pYaYa);
 
-    cout << box1.length << endl;
     return 1;
 }
