ab14979ccc9f192ac98229c7f0f0108cc9dd1175
xin49 <nnmealy@gmail.com>
2016-12-05 00:31:41 +0800

    ouo-h

--- python/gtsln/testxml.py
+++ python/gtsln/testxml.py
@@ -23,12 +23,57 @@ ele = doc.documentElement # element? element is subclass of node, inherited all 
 print(ele)
 ele.tagName
 print(ele.getAttribute("shelf"))
-print(ele.getComment())
-# 3 get the root
-
-## 2. get root
-#root = doc.documentElement
-#
+# 3 get the root use doc.documentElement get root
+root = docNew.documentElement
+# 4 read tagName, just use member tagName, can read and write it
+print(ele.tagName)
+print(root.tagName)
+print(ele.nodeName) # ele just inherited node, we can use element as node
+# 5 read content
+# 6 read attribute get attribute.value or get attr object 
+print(ele.attributes)
+attrMap = ele.attributes # NamedNodeMap -> attrMap
+attr = attrMap.item(0)
+print(attr.name)
+print(attr.value)
+print(ele.getAttribute('shelf'))
+# 7 get child with tagName
+clds = ele.getElementsByTagName('movie')
+print(clds)
+nodes = ele.childNodes # node include element and else, so can not use nodelist to get child node
+print(nodes)
+cld1 = clds[0]
+print(cld1) # you can get child element with tagName, 1 get ele-list 2 get ele
+# 8 get child with index
+# maybe you not need get child with index, but you can get the first and last child
+##fir_child = ele.firstChild
+##la_child = ele.lastChild
+##print(fir_child.data)
+##print(la_child.data) you still get the node(maybe the content node)
+# 9 get child with path(full tagName)
+# cld2 = doc.getElementsByTagName('collection.movies.type')
+# print(cld2) no
+# 10 write tagName
+ele.tagName = "baby"
+print(ele.tagName)
+print('end')
+# 11 write content
+# 12 write attribute
+ele.setAttribute('shelf', 'angerla')
+print(ele.getAttribute('shelf'))
+# 13 new node or new element
+eleNew = doc.createElement('newelement')
+print(eleNew.tagName)
+# 14 add node or element to child bottom
+ele1 = doc.createElement('newelement')
+ele2 = doc.createElement('newelement')
+ele2.appendChild(ele1)
+cld4 = ele2.getElementsByTagName('newelement')
+print(cld4)
+print(cld4[0].tagName)
+# 15 add node next
+# 16 wirte to file
+doc.writexml(open('/home/ouo/code/test/xml/pythonsave.xml', 'w'), indent="  ", addindent="  ", newl='\n')
 ## 3. get node attribute
 #if root.hasAttribute("shelf"):
 #    print("Root element: %s" % root.getAttribute("shelf"))
