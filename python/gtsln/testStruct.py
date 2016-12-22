ea60b5d1031ea547e7a6313bf364a65af1e45c5e
xin49 <nnmealy@gmail.com>
2016-12-22 22:57:47 +0800

    ouo-j

---
+++ python/gtsln/testStruct.py
@@ -0,0 +1,32 @@
+'define class '
+'1. public member'
+'2. provate member'
+'3. function'
+class TRange:
+    begin=0
+    end=0
+    def __init__(self, begin, end):
+        self.begin=begin
+        self.end=end
+
+class TTime:
+    type=0
+    range=TRange(0, 0)
+    def __init__(self, type, range):
+        self.type=type
+        self.range=range
+
+class TEmployee:
+    name=''
+    id=0
+    secureTimeLst=[]
+    unCheckTimeLst=[]
+    def __init__(self, name, id):
+        self.name=name
+        self.id=id
+def SumTimeList()
+def CalcTimeList(timeLst, CBFunc):
+    ret=0
+    for idx in timeLst:
+        ret+=CBFunc(idx.range.begin, idx.range.end)
+    return ret
