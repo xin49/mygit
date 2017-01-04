2f4cbe72fa6311da2703127051521f261936ab1d
xin49 <nnmealy@gmail.com>
2017-01-04 23:51:23 +0800

    ouo-j

--- python/gtsln/excelhelper.py
+++ python/gtsln/excelhelper.py
@@ -6,9 +6,30 @@ from xlrd import open_workbook
 from xlutils.filter import BaseReader
 import testStruct
 
+# excel helper api
+# python3 excele struct
+# workbook
+#     sheets
+#         rows
+#         cols
+#         cells
+#         
+# 1. open excel file get workbook
+# wkbook=open_workbook(fileName)
+# sheets=wkbook.sheets()
+# sheet=sheets[idx]
+# rows=sheet.get_rows()
+
 wkbook = open_workbook('/home/ouo/code/test/excel/请假报表2016.11.14.xls')
 sheets = wkbook.sheets()
 sheet_cnt = len(sheets)
+for sheet in sheets:
+    print(sheet)
+    rows=sheet.get_rows()
+    for row in rows:
+        print(row)
+        for text in row:
+            print(text)
 
 SHEEP_TYPE_UNDEF = 0
 
