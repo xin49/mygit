f79b71b945088457ee6fb5df4bdc4b4330e804b1
xin49 <nnmealy@gmail.com>
2017-04-19 17:15:15 +0800

    subcommiter:ouo time:2017年04月19日 星期三 17时15分50秒 log:     change the stringhelper

--- cpp/string/stringhelper.hpp
+++ cpp/string/stringhelper.hpp
@@ -2,24 +2,43 @@
 #include <stdio.h>
 #include <iostream>
 
+namespace stringhelper
+{
     using namespace std;
 
-    string ToHexString(string argSrc)
+    static string StringDelSym(std::string argSrc, char* pArgSym)
+    {
+        TStringList strLst;
+        df_util::StringToStringList(argSrc, pArgSym, strLst);
+        string strTmp;
+        for(int idx=0; idx<(long)strLst.size(); idx++)
+        {
+            strTmp += strLst[idx];
+        }
+        return strTmp;
+    }
+
+    static string ToHexString(string argSrc)
     {
         int strLen = argSrc.size();
         string rstStr;
         rstStr.resize(strLen*2);
-        unsigned char pHexBuf[2];
+        unsigned char pHexBuf[] = {'0', '0'};
         for(int idx=0; idx<strLen; idx++)
         {
             unsigned char byteTmp = (unsigned char)argSrc[idx];
             sprintf(pHexBuf, "%x", byteTmp);
+            if(pHexBuf[1] == 0)
+            {
+                pHexBuf[1] = pHexBuf[0];
+                pHexBuf[0] = '0';
+            }
             memcpy(&rstStr[idx*2], pHexBuf, 2);
         }
         return rstStr;
     }
 
-    string HexStringReverse(string argSrc)
+    static string HexStringReverse(string argSrc)
     {
         int srcLen = argSrc.size();
         if(srcLen%2 == 0)
@@ -37,3 +56,24 @@
         }
         return "";
     }
+
+    static string HexStringToStr(string argSrc)
+    {
+        int val;
+        int strLen = argSrc.size()/2;
+        string strRst;
+        strRst.resize(strLen);
+        for(int idx=0; idx<strLen; idx++)
+        {
+            istringstream(argSrc.substr(idx*2, 2)) >> std::hex >> val;
+            strRst[idx] = val;
+        }
+        return strRst;
+    }
+
+    static string StringReverse(string argSrc)
+    {
+        reverse(argSrc.begin(), argSrc.end());
+        return argSrc;
+    }
+}
