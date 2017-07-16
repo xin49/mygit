e22745cebcc5cd36acfec730b8af6dfa382b6820
xin49 <nnmealy@gmail.com>
2017-07-16 18:28:03 +0800

    ouo-w     learning python3 1 week

---
+++ python/tutorials/basicSyntax.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+# python identifiers: 
+#   variable/function/class/module/object name/identify 
+#       first + followed: first(litter/ _) + followed(litter/_/number)
+#   name standard:
+#       ClassName(uppercase) other with lowercase letter
+#   private element:
+#       first(_)
+#   strong private element:
+#       first(__)
+
+# reserved world:
+
+import sys
+
+try:
+    # open file stream
+    file = open(file_name, "w")
+
+except IOERROR:
+    print("There was an error writing to", file_name)
+    sys.exit()
+
+print("Enter '", file_finish,)
+print  "' When finished"
